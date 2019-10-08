75f330e86792e88320b4be7285b96eaf9760ff50
jorgeandres6 <jorgeandres6@msn.com>
2019-10-08 14:49:42 -0500

    routing y materialize

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'login', component: LoginComponent},
+  { path: 'main', component: MainComponent},
+  { path: '',   redirectTo: '/login', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
